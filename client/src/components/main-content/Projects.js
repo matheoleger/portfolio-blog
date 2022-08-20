eb185ba00ee5de7397b1447b39ec7c69c00a0180
matheoleger <71370253+matheoleger@users.noreply.github.com>
2022-08-20 20:48:20 +0200

    Added data for formation and experiences

--- client/src/components/main-content/Projects.js
+++ client/src/components/main-content/Projects.js
@@ -2,10 +2,19 @@ import React from 'react';
 
 import "../../css/Projects.css"
 
+import Github from "../../img/github.png"
+
 function Projects() {
     return (
         <section class="projects-section main-section" id="projects">
             <h1>PROJETS</h1>
+            <div className="projects-content">
+                <p>Il n'y a pour l'instant pas de projets affichés ici, vous pouvez retrouver mes projets ici :</p>
+                <a href="https://github.com/matheoleger" target="_blank">
+                    <img src={Github}/>
+                    <h3>Github</h3>
+                </a>
+            </div>
         </section>
     )
 }

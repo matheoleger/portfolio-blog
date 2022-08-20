a847526e70e4f03f96faa836a20406957acc5602
matheoleger <71370253+matheoleger@users.noreply.github.com>
2022-08-20 15:29:44 +0200

    fix(server.ts): I changed the order of the file to fix problem of path for API
    The path of the API must be before '/*' path to define where we can get data

--- server.ts
+++ server.ts
@@ -9,12 +9,6 @@ import {getCompany} from "./src/getCompany"
 const app = express();
 app.use(express.json())
 
-app.use(express.static(path.join(__dirname, "../client/build")));
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-});
-
 app.get("/api", (req, res) => {
     res.json({"users": ["userOne", "userTwo", "userThree"]});
 });
@@ -44,6 +38,12 @@ app.get("/api/post/:id", async (req, res) => {
     const {id} = req.params;
     res.json(await getPost(id));
 })
+
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Server started on port 5000");
